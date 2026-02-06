3b73b95406ef35d6929564c641f856466ec296f5
gabrielfelice <gabifelice@gmail.com>
2026-02-06 12:18:08 +0000

    configurações iniciais e primeira página

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <h1>Isso será um Sudoku</h1>
+      <p>Em breve você poderá jogar aqui.</p>
+      <p>Aguarde e verá!.</p>
+    </>
+  );
+}
+
+export default Home;
